48ee2af3ce7e5463a743a6bf7fc192a2246af5d3
Cole Barach <colebarach@tutanota.com>
2026-09-07 18:19:09 -0400

    Ported the controller interface from the retro console project. Reworked the polymorphism somewhat, however I'm still not settled on the current version.

---
+++ src/controller/controller_nes.c
@@ -0,0 +1,173 @@
+// Header
+#include "controller_nes.h"
+
+// Includes -------------------------------------------------------------------------------------------------------------------
+
+// RPi Pico SDK
+#include <pico/stdlib.h>
+
+// Constants ------------------------------------------------------------------------------------------------------------------
+
+#define CONTROLLER_NES_A		0b00000001
+#define CONTROLLER_NES_B		0b00000010
+#define CONTROLLER_NES_SELECT	0b00000100
+#define CONTROLLER_NES_START	0b00001000
+#define CONTROLLER_NES_UP		0b00010000
+#define CONTROLLER_NES_DOWN		0b00100000
+#define CONTROLLER_NES_LEFT		0b01000000
+#define CONTROLLER_NES_RIGHT	0b10000000
+
+// Functions ------------------------------------------------------------------------------------------------------------------
+
+void controllerNesInit (controllerNes_t* controller, const controllerNesConfig_t* config)
+{
+	// Init the struct
+	*controller = (controllerNes_t)
+	{
+		.vmt =
+		{
+			.read			= controllerNesRead,
+			.buttonPressed	= controllerNesButtonPressed,
+			.buttonHeld		= controllerNesButtonHeld
+		},
+		.config = config
+	};
+
+	// Initialize GPIO
+	gpio_init (controller->config->pinSerialData);
+	gpio_init (controller->config->pinSerialClock);
+	gpio_init (controller->config->pinSerialLatch);
+	gpio_set_dir (controller->config->pinSerialData,  GPIO_IN);
+	gpio_set_dir (controller->config->pinSerialClock, GPIO_OUT);
+	gpio_set_dir (controller->config->pinSerialLatch, GPIO_OUT);
+
+	// Default to no input
+	// - Pull-up resistor is not be required for a connected controller, but is for an unconnected controller.
+	gpio_pull_up (nes->config.pinSerialData);
+}
+
+void controllerNesRead (void* controller)
+{
+	controllerNes_t* nes = controller;
+
+	uint32_t clockPeriodHalf = nes->config->clockPeriod / 2;
+
+	uint8_t pressed = 0x00;
+
+	// Send latch signal, sample 'A' button
+	gpio_put (nes->config->pinSerialLatch, true);
+	sleep_us (clockPeriodHalf);
+	pressed |= !gpio_get (nes->config->pinSerialData);
+	gpio_put (nes->config->pinSerialLatch, false);
+	sleep_us (clockPeriodHalf);
+
+	// Read 'B' button
+	gpio_put (nes->config->pinSerialClock, true);
+	sleep_us (clockPeriodHalf);
+	pressed |= !gpio_get (nes->config->pinSerialData) << 1;
+	gpio_put (nes->config->pinSerialClock, false);
+	sleep_us (clockPeriodHalf);
+
+	// Get select button
+	gpio_put (nes->config->pinSerialClock, true);
+	sleep_us (clockPeriodHalf);
+	pressed |= !gpio_get (nes->config->pinSerialData) << 2;
+	gpio_put (nes->config->pinSerialClock, false);
+	sleep_us (clockPeriodHalf);
+
+	// Get start button
+	gpio_put (nes->config->pinSerialClock, true);
+	sleep_us (clockPeriodHalf);
+	pressed |= !gpio_get (nes->config->pinSerialData) << 3;
+	gpio_put (nes->config->pinSerialClock, false);
+	sleep_us (clockPeriodHalf);
+
+	// Get up button
+	gpio_put (nes->config->pinSerialClock, true);
+	sleep_us (clockPeriodHalf);
+	pressed |= !gpio_get (nes->config->pinSerialData) << 4;
+	gpio_put (nes->config->pinSerialClock, false);
+	sleep_us (clockPeriodHalf);
+
+	// Get down button
+	gpio_put (nes->config->pinSerialClock, true);
+	sleep_us (clockPeriodHalf);
+	pressed |= !gpio_get (nes->config->pinSerialData) << 5;
+	gpio_put (nes->config->pinSerialClock, false);
+	sleep_us (clockPeriodHalf);
+
+	// Get left button
+	gpio_put (nes->config->pinSerialClock, true);
+	sleep_us (clockPeriodHalf);
+	pressed |= !gpio_get (nes->config->pinSerialData) << 6;
+	gpio_put (nes->config->pinSerialClock, false);
+	sleep_us (clockPeriodHalf);
+
+	// Get right button
+	gpio_put (nes->config->pinSerialClock, true);
+	sleep_us (clockPeriodHalf);
+	pressed |= !gpio_get (nes->config->pinSerialData) << 7;
+	gpio_put (nes->config->pinSerialClock, false);
+	sleep_us (clockPeriodHalf);
+
+	nes->buttonsHeld = nes->buttonsPressed & pressed;
+	nes->buttonsPressed = pressed & ~nes->buttonsHeld;
+}
+
+bool controllerNesButtonPressed (void* controller, controllerButton_t button)
+{
+	controllerNes_t* nes = controller;
+
+	switch (button)
+	{
+		case CONTROLLER_BUTTON_A:
+			return nes->buttonsPressed & CONTROLLER_NES_A;
+		case CONTROLLER_BUTTON_B:
+			return nes->buttonsPressed & CONTROLLER_NES_B;
+		case CONTROLLER_BUTTON_SELECT:
+			return nes->buttonsPressed & CONTROLLER_NES_SELECT;
+		case CONTROLLER_BUTTON_START:
+			return nes->buttonsPressed & CONTROLLER_NES_START;
+		case CONTROLLER_BUTTON_UP:
+			return nes->buttonsPressed & CONTROLLER_NES_UP;
+		case CONTROLLER_BUTTON_DOWN:
+			return nes->buttonsPressed & CONTROLLER_NES_DOWN;
+		case CONTROLLER_BUTTON_LEFT:
+			return nes->buttonsPressed & CONTROLLER_NES_LEFT;
+		case CONTROLLER_BUTTON_RIGHT:
+			return nes->buttonsPressed & CONTROLLER_NES_RIGHT;
+		default:
+			break;
+	}
+
+	return false;
+}
+
+bool controllerNesButtonHeld (void* controller, controllerButton_t button)
+{
+	controllerNes_t* nes = controller;
+
+	switch (button)
+	{
+		case CONTROLLER_BUTTON_A:
+			return nes->buttonsHeld & CONTROLLER_NES_A;
+		case CONTROLLER_BUTTON_B:
+			return nes->buttonsHeld & CONTROLLER_NES_B;
+		case CONTROLLER_BUTTON_SELECT:
+			return nes->buttonsHeld & CONTROLLER_NES_SELECT;
+		case CONTROLLER_BUTTON_START:
+			return nes->buttonsHeld & CONTROLLER_NES_START;
+		case CONTROLLER_BUTTON_UP:
+			return nes->buttonsHeld & CONTROLLER_NES_UP;
+		case CONTROLLER_BUTTON_DOWN:
+			return nes->buttonsHeld & CONTROLLER_NES_DOWN;
+		case CONTROLLER_BUTTON_LEFT:
+			return nes->buttonsHeld & CONTROLLER_NES_LEFT;
+		case CONTROLLER_BUTTON_RIGHT:
+			return nes->buttonsHeld & CONTROLLER_NES_RIGHT;
+		default:
+			break;
+	}
+
+	return false;
+}
---
+++ src/controller/controller_nes.h
@@ -0,0 +1,95 @@
+#ifndef CONTROLLER_NES_H
+#define CONTROLLER_NES_H
+
+// NES Controller -------------------------------------------------------------------------------------------------------------
+//
+// Author: Cole Barach
+// Date Created: 2023.12.01
+//
+// TODO(Barach): How to make polymorphic without overhead?
+//
+// Description: Bit-banging implementation of an interface for the Nintendo Entertainment System controllers. Each controller
+//   has 8 buttons. The controllers use a modified version of SPI to communicate, acting as a secondary device. The layout of
+//   the controller and the pinout of the connector is given below.
+//
+// NES Controller Button Naming Scheme:
+// ___________________________________
+// |                                 |   0 - 'A'       4 - Up
+// |   4                             |   1 - 'B'       5 - Down
+// | 6   7   Select  Start   1  0    |   2 - Select    6 - Left
+// |   5       2       3     B  A    |   3 - Start     7 - Right
+// |_________________________________|
+//
+// NES Controller Female Connector (viewed with cable facing away):
+//  _______
+// /    7 |    1 - VCC                   5 - Serial latch
+// | 1  6 |    2 - No connection         6 - Serial clock
+// | 2  5 |    3 - No connection         7 - Ground
+// | 3  4 |    4 - Serial data out
+// |______|
+//
+// NES Controller Protocol: As mentioned previously, the NES controller uses a modified version of SPI. The only difference
+//   between the NES's protocol and SPI is that the communication is started using a latch signal. The latch signal is then
+//   followed by 7 clock pulses. A timing diagram for the communication is listed below.
+//
+//   NES Controller Timing Diagram:
+//              ____
+//     Latch: __|  |____________________________________________
+//                    ____  ____  ____  ____  ____  ____  ____
+//     Clock: ________|  |__|  |__|  |__|  |__|  |__|  |__|  |__
+//              ________________________________________________
+//     Data:  __| D0  | D1  | D2  | D3  | D4  | D5  | D6  | D7
+//
+//   Each bit of the data byte indicates whether the correlating button is held down or not. If a bit is high, then the
+//   corresponding button is not pressed, if the bit is low, the corresponding button is pressed. Each bit maps directly to
+//   the buttons in the order they are named above (I.E. D0 ~ Button 0).
+//
+//   According to the datasheet for the CD4021 shift register (the IC used by the controllers), the clock frequency may vary
+//   from [0Hz, 2.5MHz], although this is dependent on the supply voltage.
+//
+//   The CD4021 may be powered by any voltage in the range of [3V, 15V], in this case 3.3V was chosen for convenience.
+//
+// References:
+// - https://www.allaboutcircuits.com/projects/nes-controller-interface-with-an-arduino-uno/
+
+// Includes -------------------------------------------------------------------------------------------------------------------
+
+// Includes
+#include "controller.h"
+
+// Datatypes ------------------------------------------------------------------------------------------------------------------
+
+typedef struct controllerNesConfig
+{
+	/// @brief The GPIO pin tied to the serial data pin. Initialized and configured as an input with software pull up.
+	uint32_t pinSerialData;
+
+	/// @brief The GPIO pin tied to the serial clock pin. Initialized and configured as an output.
+	uint32_t pinSerialClock;
+
+	/// @brief The GPIO pin tied to the serial latch pin. Initialized and configured as an output.
+	uint32_t pinSerialLatch;
+
+	/// @brief The period to pulse the clock line at, in microseconds. For example, 40 => 25 kHz.
+	uint32_t clockPeriod;
+} controllerNesConfig_t;
+
+typedef struct controllerNes
+{
+	controllerVmt_t vmt;
+	const controllerNesConfig_t* config;
+	uint8_t buttonsPressed;
+	uint8_t buttonsHeld;
+} controllerNes_t;
+
+// Functions ------------------------------------------------------------------------------------------------------------------
+
+void controllerNesInit (controllerNes_t* controller, const controllerNesConfig_t* config);
+
+void controllerNesRead (void* controller);
+
+bool controllerNesButtonPressed (void* controller, controllerButton_t button);
+
+bool controllerNesButtonHeld (void* controller, controllerButton_t button);
+
+#endif // CONTROLLER_NES_H
